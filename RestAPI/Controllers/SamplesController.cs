22185516674a022e6f50a17c991060210a3c0dda
Admin <Admin@DESKTOP-1G3GB48>
2018-10-27 19:30:14 +0530

    Add project files.

---
+++ RestAPI/Controllers/SamplesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestAPI.Models;
+using System.Web.SessionState;
+using System.Web.Mvc;
+
+namespace RestAPI.Controllers
+{
+    public class SamplesController : ApiController
+    {
+        private RestAPIContext db = new RestAPIContext();
+
+        // GET: api/Samples
+        public IQueryable<Sample> GetSamples()
+        {
+            return db.Samples;
+        }
+
+        // GET: api/Samples/5
+        [ResponseType(typeof(Sample))]
+        public IHttpActionResult GetSample(string id)
+        {
+            Sample sample = db.Samples.Find(id);
+            if (sample == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sample);
+        }
+
+        // PUT: api/Samples/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSample(string id, Sample sample)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sample.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(sample).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SampleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Samples
+        [ResponseType(typeof(Sample))]
+        public HttpResponseMessage PostSample(List<Sample> sampleList)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpResponseMessage { StatusCode = HttpStatusCode.BadRequest };
+            }
+
+            foreach (var sample in sampleList)
+            {
+                db.Samples.Add(sample);
+            }
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+
+                throw;
+            }
+
+            return new HttpResponseMessage { StatusCode = HttpStatusCode.Created };
+        }
+
+        // DELETE: api/Samples/5
+        [ResponseType(typeof(Sample))]
+        public IHttpActionResult DeleteSample(string id)
+        {
+            Sample sample = db.Samples.Find(id);
+            if (sample == null)
+            {
+                return NotFound();
+            }
+
+            db.Samples.Remove(sample);
+            db.SaveChanges();
+
+            return Ok(sample);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SampleExists(string id)
+        {
+            return db.Samples.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ RestAPI/Models/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestAPI.Models
+{
+    public class Sample
+    {
+        public object Id { get; set; }
+        [Key]
+        public int ApplicationId { get; set; }
+        public string Type { get; set; }
+        public string Summary { get; set; }
+        public double Amount { get; set; }
+        public DateTime PostingDate { get; set; }
+        public bool IsCleared { get; set; }
+        public DateTime? ClearedDate { get; set; }
+    }
+}
